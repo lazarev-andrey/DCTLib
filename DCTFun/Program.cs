255bf3d2dff5f25cc53cb0b7b5e431ec893e0e95
ruarai <ruarai.big@gmail.com>
2015-06-13 22:00:37 +1000

    init

---
+++ DCTFun/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DCTLib;
+
+namespace DCTFun
+{
+    class Program
+    {
+        private static int size = 8;
+        private const int imageSize = 1024;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What size should the matrices be? Roughly 8-32. Larger numbers take longer.");
+
+            size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Starting...");
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            //input bitmap
+            Bitmap testImage = new Bitmap("test.png");
+
+            //output bitmaps and graphics
+            Bitmap cycleBitmap = new Bitmap(imageSize, imageSize);
+            Graphics cycleGraphics = Graphics.FromImage(cycleBitmap);
+
+            Bitmap coeffBitmap = new Bitmap(imageSize, imageSize);
+            Graphics coeffGraphics = Graphics.FromImage(coeffBitmap);
+
+            //instance of dct we will use
+            DCT d = new DCT(size);
+
+            for (int y = 0; y < imageSize / size; y++)
+            {
+                for (int x = 0; x < imageSize / size; x++)
+                {
+                    Bitmap sector = new Bitmap(size, size);
+                    Graphics g = Graphics.FromImage(sector);
+
+                    //where bitmap data will be copied
+                    Rectangle dest = new Rectangle(0, 0, size, size);
+                    Rectangle src = new Rectangle(x * size, y * size, size, size);
+
+                    g.DrawImage(testImage, dest, src, GraphicsUnit.Pixel);
+
+                    double[][,] coeffs = d.BitmapToMatrices(sector);
+                    d.DCTMatrices(coeffs);//Pass with DCT
+
+                    coeffGraphics.DrawImage(d.MatricesToBitmap((coeffs)), src, dest, GraphicsUnit.Pixel);//Draw the coefficient table
+
+
+                    d.IDCTMatrices(coeffs);//Pass with IDCT
+                    cycleGraphics.DrawImage(d.MatricesToBitmap(coeffs), src, dest, GraphicsUnit.Pixel);//Draw a returned cycle
+
+
+                }
+                Console.WriteLine("{0} of {1}", y, imageSize / size);
+            }
+
+            Console.WriteLine(s.ElapsedMilliseconds);
+            cycleBitmap.Save("cycled.png");
+            coeffBitmap.Save("coeffs.png");
+            Console.WriteLine("Complete. Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DCTLib/DCT.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTLib
+{
+    public class DCT
+    {
+        public DCT(int matrixSize)
+        {
+            size = matrixSize;
+        }
+
+        //size of all matrices, width and height.
+        private readonly int size;
+
+        //Turn DCT matrices into an RGB bitmap for output
+        public Bitmap MatricesToBitmap(double[][,] array)
+        {
+            Bitmap b = new Bitmap(size, size);
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    byte R = (byte)(array[0][x, y]);
+                    byte G = (byte)(array[1][x, y]);
+                    byte B = (byte)(array[2][x, y]);
+                    b.SetPixel(x, y, Color.FromArgb(R, G, B));
+                }
+            }
+            return b;
+        }
+
+        //Create matrices from an RGB bitmap
+        public double[][,] BitmapToMatrices(Bitmap b)
+        {
+            double[][,] matrices = new double[3][,];
+
+            for (int i = 0; i < 3; i++)
+            {
+                matrices[i] = new double[size, size];
+            }
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    matrices[0][x, y] = b.GetPixel(x, y).R / 255d;
+                    matrices[1][x, y] = b.GetPixel(x, y).G / 255d;
+                    matrices[2][x, y] = b.GetPixel(x, y).B / 255d;
+                }
+            }
+            return matrices;
+        }
+
+        //Run the DCT2D on 3-channeled group of matrices
+        public void DCTMatrices(double[][,] matrices)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                matrices[i] = DCT2D(matrices[i]);
+            }
+        }
+
+        //Run the inverse DCT2D on 3-channeled group of matrices
+        public void IDCTMatrices(double[][,] matrices)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                matrices[i] = IDCT2D(matrices[i]);
+            }
+        }
+
+        //Run a DCT2D on a single matrix
+        public double[,] DCT2D(double[,] input)
+        {
+            double[,] coeffs = new double[size, size];
+
+            //To initialise every [u,v] value in the coefficient table...
+            for (int u = 0; u < size; u++)
+            {
+                for (int v = 0; v < size; v++)
+                {
+                    //...sum the basisfunction for every [x,y] value in the bitmap input
+                    double sum = 0d;
+
+                    for (int x = 0; x < size; x++)
+                    {
+                        for (int y = 0; y < size; y++)
+                        {
+                            double a = input[x, y];
+                            sum += BasisFunction(a, u, v, x, y);
+                        }
+                    }
+                    coeffs[u, v] = sum;
+                }
+            }
+            return coeffs;
+        }
+
+        //Run an inverse DCT2D on a single matrix
+        public double[,] IDCT2D(double[,] coeffs)
+        {
+            double[,] output = new double[size, size];
+
+            //To initialise every [x,y] value in the bitmap output...
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    //...sum the basisfunction for every [u,v] value in the coefficient table
+                    double sum = 0d;
+
+                    for (int u = 0; u < size; u++)
+                    {
+                        for (int v = 0; v < size; v++)
+                        {
+                            double a = coeffs[u, v];
+                            sum += BasisFunction(a, u, v, x, y);
+                        }
+                    }
+
+                    output[x, y] = sum;
+                }
+            }
+            return output;
+        }
+
+        //The basis function for the DCT
+        //a is the value of the matrix cell in question
+        public double BasisFunction(double a, double u, double v, double x, double y)
+        {
+            double b = Math.Cos((Math.PI * (x + 0.5d) * u) / size);
+            double c = Math.Cos((Math.PI * (y + 0.5d) * v) / size);
+
+            double d = 16d / size;//normally listed as constant '2', but this accounts for change in matrix size. not sure why.
+
+            return a*b*c*d;
+        }
+    }
+}
