dcaecc2172a0ef053aa204354855985edfc134f8
ruarai <ruarai.big@gmail.com>
2015-06-14 12:14:11 +1000

    improve test app

--- DCTFun/Program.cs
+++ DCTFun/Program.cs
@@ -11,33 +11,42 @@ namespace DCTFun
 {
     class Program
     {
-        private static int size = 8;
-        private const int imageSize = 1024;
+        private static int size;
         static void Main(string[] args)
         {
-            Console.WriteLine("What size should the matrices be? Roughly 8-32. Larger numbers take longer.");
+            if (Debugger.IsAttached)
+                args = new[] {"test.png"};
 
-            size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input: {0}", args[0]);
+            Console.WriteLine("What size should the matrices be? Roughly 2-32. Larger numbers take longer.");
+            Console.WriteLine("Best results are multiples of the input image's width and height.");
+
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("Input should be a whole positive number.");
+            }
 
             Console.WriteLine("Starting...");
             Stopwatch s = new Stopwatch();
             s.Start();
             //input bitmap
-            Bitmap testImage = new Bitmap("test.png");
+            Bitmap bitmap = new Bitmap(args[0]);
+            int width = bitmap.Width;
+            int height = bitmap.Height;
 
             //output bitmaps and graphics
-            Bitmap cycleBitmap = new Bitmap(imageSize, imageSize);
+            Bitmap cycleBitmap = new Bitmap(width, height);
             Graphics cycleGraphics = Graphics.FromImage(cycleBitmap);
 
-            Bitmap coeffBitmap = new Bitmap(imageSize, imageSize);
+            Bitmap coeffBitmap = new Bitmap(width, height);
             Graphics coeffGraphics = Graphics.FromImage(coeffBitmap);
 
             //instance of dct we will use
             DCT d = new DCT(size);
 
-            for (int y = 0; y < imageSize / size; y++)
+            for (int y = 0; y < height / size; y++)
             {
-                for (int x = 0; x < imageSize / size; x++)
+                for (int x = 0; x < width / size; x++)
                 {
                     Bitmap sector = new Bitmap(size, size);
                     Graphics g = Graphics.FromImage(sector);
@@ -46,7 +55,7 @@ namespace DCTFun
                     Rectangle dest = new Rectangle(0, 0, size, size);
                     Rectangle src = new Rectangle(x * size, y * size, size, size);
 
-                    g.DrawImage(testImage, dest, src, GraphicsUnit.Pixel);
+                    g.DrawImage(bitmap, dest, src, GraphicsUnit.Pixel);
 
                     double[][,] coeffs = d.BitmapToMatrices(sector);
                     d.DCTMatrices(coeffs);//Pass with DCT
@@ -59,13 +68,19 @@ namespace DCTFun
 
 
                 }
-                Console.WriteLine("{0} of {1}", y, imageSize / size);
+                Console.Write("\r{0} of {1}", y, height / size);
             }
+            Console.WriteLine();
+            Console.WriteLine("{0} seconds elapsed. ", s.ElapsedMilliseconds / 1000);
 
-            Console.WriteLine(s.ElapsedMilliseconds);
+
             cycleBitmap.Save("cycled.png");
+            Console.WriteLine("Saved cycled.png");
             coeffBitmap.Save("coeffs.png");
-            Console.WriteLine("Complete. Press any key to continue.");
+            Console.WriteLine("Saved coeffs.png");
+
+
+            Console.WriteLine("Complete. Press any key to exit.");
             Console.ReadKey();
         }
     }
