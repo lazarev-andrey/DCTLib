fde793124813eb8984cab7cf6f20d4e3a5c4c7b6
ruarai <ruarai.big@gmail.com>
2015-06-16 20:50:15 +1000

    make DCT per standards

--- DCTLib/DCT.cs
+++ DCTLib/DCT.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DCTLib
@@ -20,18 +21,24 @@ namespace DCTLib
         //Turn DCT matrices into an RGB bitmap for output
         public Bitmap MatricesToBitmap(double[][,] array)
         {
+            PrintMatrix(array[0]);
             Bitmap b = new Bitmap(size, size);
             for (int x = 0; x < size; x++)
             {
                 for (int y = 0; y < size; y++)
                 {
-                    byte R = (byte)(array[0][x, y]);
-                    byte G = (byte)(array[1][x, y]);
-                    byte B = (byte)(array[2][x, y]);
+                    byte R = (byte)(normOut(array[0][x, y] + 128));
+                    byte G = (byte)(normOut(array[1][x, y] + 128));
+                    byte B = (byte)(normOut(array[2][x, y] + 128));
                     b.SetPixel(x, y, Color.FromArgb(R, G, B));
                 }
             }
             return b;
+        }
+
+        private double normOut(double a)
+        {
+            return Math.Min(Math.Max(a, 0), 255);
         }
 
         //Create matrices from an RGB bitmap
@@ -48,11 +55,12 @@ namespace DCTLib
             {
                 for (int y = 0; y < size; y++)
                 {
-                    matrices[0][x, y] = b.GetPixel(x, y).R / 255d;
-                    matrices[1][x, y] = b.GetPixel(x, y).G / 255d;
-                    matrices[2][x, y] = b.GetPixel(x, y).B / 255d;
+                    matrices[0][x, y] = b.GetPixel(x, y).R - 128;
+                    matrices[1][x, y] = b.GetPixel(x, y).G - 128;
+                    matrices[2][x, y] = b.GetPixel(x, y).B - 128;
                 }
             }
+            PrintMatrix(matrices[0]);
             return matrices;
         }
 
@@ -87,7 +95,7 @@ namespace DCTLib
                     //...sum the basisfunction for every [x,y] value in the bitmap input
                     double sum = 0d;
 
-                    
+
 
                     for (int x = 0; x < size; x++)
                     {
@@ -97,7 +105,7 @@ namespace DCTLib
                             sum += BasisFunction(a, u, v, x, y);
                         }
                     }
-                    coeffs[u, v] = sum;
+                    coeffs[u, v] = sum * 0.25d * alpha(u) * alpha(v);
                 }
             }
             return coeffs;
@@ -121,26 +129,44 @@ namespace DCTLib
                         for (int v = 0; v < size; v++)
                         {
                             double a = coeffs[u, v];
-                            sum += BasisFunction(a, u, v, x, y);
+                            sum += BasisFunction(a, u, v, x, y) * alpha(u) * alpha(v);
                         }
                     }
 
-                    output[x, y] = sum;
+                    output[x, y] = sum * 0.25d;
                 }
             }
             return output;
         }
 
-        //The basis function for the DCT
-        //a is the value of the matrix cell in question
         public double BasisFunction(double a, double u, double v, double x, double y)
         {
-            double b = Math.Cos((Math.PI * (x + 0.5d) * u) / size);
-            double c = Math.Cos((Math.PI * (y + 0.5d) * v) / size);
+            double b = Math.Cos(((2d * x + 1d) * u * Math.PI) / 16d);
+            double c = Math.Cos(((2d * y + 1d) * v * Math.PI) / 16d);
 
-            double d = 16d / size;//normally listed as constant '2', but this accounts for change in matrix size. not sure why.
-
-            return a*b*c*d;
+            return a * b * c;
         }
+
+
+        private void PrintMatrix(double[,] matrix)
+        {
+            return;
+            for (int u = 0; u < size; u++)
+            {
+                for (int v = 0; v < size; v++)
+                {
+                    Console.Write(Math.Round(matrix[u, v], 2) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private double alpha(int u)
+        {
+            if (u == 0)
+                return 1 / Math.Sqrt(2);
+            return 1;
+        }
+
     }
 }
